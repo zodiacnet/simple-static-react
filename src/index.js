5bd7f8ea7c197c0f08d40bbc777aaddbb7c636f6
hector aguirre <zdissonance@gmail.com>
2019-07-22 23:10:20 +0000

    test autoplay

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ class HelloWorld extends Component{
           </li>
         </ul>
 
-	<iframe width="560" height="315" src="https://www.youtube.com/embed/o0Zl3NwjGwc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/o0Zl3NwjGwc?autoplay=1&rel=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
       </div>
     )
