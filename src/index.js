5383fb7860747e63f252b2576755d7bf0c7be773
Hector Aguirre <hecdan8@hotmail.com>
2019-07-22 20:59:15 +0000

    add gif

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ class HelloWorld extends Component{
           <li>
             Run 'npm run build' before uploading your public directory to your website.
           </li>
+	    <div style='position:relative; padding-bottom:calc(56.25% + 44px)'><iframe src='https://gfycat.com/ifr/SevereSmugHoneyeater' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0;' allowfullscreen></iframe></div><p> <a href="https://gfycat.com/severesmughoneyeater">via Gfycat</a></p>
         </ul>
       </div>
     )
