d1bfb23a061e227a21ba0b1ad9a3b75316448816
hector aguirre <zdissonance@gmail.com>
2019-07-23 00:34:06 +0000

    add another image

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ class HelloWorld extends Component{
             Run 'npm run build' before uploading your public directory to your website.
           </li>
         </ul>
-	<iframe src='https://gfycat.com/ifr/SevereSmugHoneyeater' frameborder='0' scrolling='no' allowfullscreen width='640' height='404'></iframe><p> <a href="https://gfycat.com/severesmughoneyeater"></a></p>
+	<img src="https://i.pinimg.com/originals/f2/52/42/f252421d0e8e5046ef2a7510dc381cfe.jpg"/>
       </div>
     )
   }
